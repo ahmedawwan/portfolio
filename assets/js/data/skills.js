5ccee07aae589d2c9e94a7fe46f44f7169858c2c
ahmedawwan <ahmedsaeed0973@gmail.com>
2026-03-16 15:50:03 +0500

    updated skills

--- assets/js/data/skills.js
+++ assets/js/data/skills.js
@@ -1,11 +1,12 @@
 window.skillsData = [
   { name: "Flutter", value: 90 },
   { name: "Dart", value: 90 },
+  { name: "Python", value: 80 },
+  { name: "Django", value: 75 },
+  { name: "FastAPI", value: 70 },
+  { name: "SQL", value: 70 },
   { name: "Firebase", value: 90 },
-  { name: "Django & Python", value: 75 },
   { name: "Node js, Express js & MongoDB", value: 50 },
-  { name: "React Native", value: 50 },
+  // { name: "React Native", value: 50 },
   { name: "Wordpress", value: 80 },
-  { name: "SQL", value: 60 },
 ];
-
